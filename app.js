2a24e395247a60285bd56465d502b94ccddce87f
Felipe Astroza <felipe@astroza.cl>
2016-10-31 18:54:36 -0300

    graph

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ var parser;
 var sampling;
 var log_arr = [];
 var db = new Database();
-
+var test_mode = false;
+var graph_data = {};
+var graph_size = 100;
+var line_graph;
 
 function scan_devices(cb) {
   serialport.list(function(err, devices) {
@@ -44,6 +47,17 @@ function app_log(line) {
 function on_serial_data(line) {
   var values = line.split(", ");
   app_log("Channel 0: " + values[0] + ", Channel 1: " + values[1]);
+  var s0 = graph_data.values[0];
+  var s1 = graph_data.values[1];
+  if(s0.length >= graph_size) {
+    s0.shift();
+    s1.shift();
+    graph_data.start += graph_data.step;
+  }
+  s0.push(parseFloat(values[0]));
+  s1.push(parseFloat(values[1]));
+  graph_data.end += graph_data.step;
+  line_graph.updateData(graph_data);
   console.log("DB");
   db.insertCurrentSample(values);
 }
@@ -66,6 +80,7 @@ function connect_device(button) {
       sampling = setInterval(take_sample, parseInt($("#sample_time").val()) * 1000);
       app_log("Connected!");
       app_log("Sampling every " + $("#sample_time").val() + " second(s)");
+      init_graph(parseInt($("#sample_time").val()) * 1000);
     }
   });
 }
@@ -106,6 +121,29 @@ function export_to_csv(path) {
   });
 }
 
+function simulate_data() {
+  $(".stage0").hide();
+  $(".stage1").show();
+  setInterval(function() {
+    var ch0 = (Math.random() * 100).toFixed(2);
+    var ch1 = (Math.random() * 100).toFixed(2);
+    var data = ch0 + ", " + ch1;
+    on_serial_data(data);
+  }, 1000);
+  init_graph(1000);
+}
+
+function init_graph(step) {
+  graph_data.names = ["chan 0", "chan 1"];
+  graph_data.colors = ["green","orange"];
+  graph_data.scale = "linear";
+  graph_data.start = +new Date();
+  graph_data.end = graph_data.start;
+  graph_data.values = [[], []];
+  graph_data.step = step;
+  line_graph = new LineGraph({containerId: 'graph1', data: graph_data});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   $(".stage1").hide();
   $("#error_msg").hide();
@@ -116,6 +154,8 @@ document.addEventListener('DOMContentLoaded', function() {
     $(this).val('');
   });
   $("#export_from").val(moment().format("YYYY-MM-DD"));
+  if(test_mode)
+    simulate_data();
 });
 
 var win = require('nw.gui').Window.get();
